bfb742a77fa2d006770d8701630222ba09b1867b
Sreejitha R <gautamsreejitha1@gmail.com>
2023-02-11 02:16:19 +0530

    Create StringToArray.java

---
+++ src/main/java/com/basic/leetpractice/StringToArray.java
@@ -0,0 +1,10 @@
+package com.basic.leetpractice;
+
+public class StringToArray {
+    public static void main(String[] args) {
+        String s = "sreejitha";
+        char[] har = s.toCharArray();
+
+        System.out.println(har);
+    }
+}
