dc9f02a49d923dc9ff99628393bc30ac18b47c76
Sreejitha R <gautamsreejitha1@gmail.com>
2023-02-11 21:59:37 +0530

    Create RemoveDuplicates.java

---
+++ src/main/java/com/basic/leetpractice/RemoveDuplicates.java
@@ -0,0 +1,33 @@
+
+
+//remove all duplicates from a given string
+package com.basic.leetpractice;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+public class RemoveDuplicates {
+    static void remove(String s){
+        StringBuilder builder = new StringBuilder();
+        Set<Character> set = new HashSet<>();
+        char[] chars = s.toCharArray();
+        for(char c: chars) {
+            if (set.add(c)) {
+                builder.append(c);
+
+            }
+        }
+            System.out.println(builder.toString());
+
+
+
+
+
+    }
+    public static void main(String[] args) {
+        String str = "Sreejitha";
+        remove(str);
+
+    }
+}
