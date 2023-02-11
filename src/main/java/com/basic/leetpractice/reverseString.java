547ddd1bf2bcd7f110e2804fcba69a8cfde683b6
Sreejitha R <gautamsreejitha1@gmail.com>
2023-02-11 23:59:57 +0530

    Update reverseString.java

--- src/main/java/com/basic/leetpractice/reverseString.java
+++ src/main/java/com/basic/leetpractice/reverseString.java
@@ -3,15 +3,17 @@ package com.basic.leetpractice;
 public class reverseString {
     static void rvrs(String s){
 
-       StringBuffer sb = new StringBuffer(s);
-       System.out.println( sb.reverse());
+     StringBuffer sb = new StringBuffer(s);
+      // System.out.println( sb.reverse());
         char[] ch= s.toCharArray();
-        String str = new String();
+        String str = "";
         for(int i =s.length()-1;i>=0;i--){
-            str = str+  ch[i];
-
+           str = str+  ch[i];
+           // sb.append(ch[i]);
         }
-        System.out.println(str);
+       System.out.println(str);
+      //  System.out.println(sb.toString());
+
     }
     public static void main(String[] args) {
         rvrs("work");
