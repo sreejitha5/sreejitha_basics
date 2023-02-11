2cd379c4729b4a2ff6927da5fd975bc6ebadb134
Sreejitha R <gautamsreejitha1@gmail.com>
2023-02-12 01:42:28 +0530

    Create NonRepeat.java

---
+++ src/main/java/com/basic/leetpractice/NonRepeat.java
@@ -0,0 +1,21 @@
+package com.basic.leetpractice;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class NonRepeat {
+    public static void main(String[] args) {
+        String s = "java";
+        StringBuffer sb = new StringBuffer();
+        char[]  chars= s.toCharArray();
+        Set<Character> set = new HashSet<>();
+
+        for(char c:chars){
+            if(set.add(c)){
+                sb.append(c);break;
+            }
+        }
+        System.out.println(sb.toString());
+        System.out.println(sb.substring(0,1) );
+        }
+    }
